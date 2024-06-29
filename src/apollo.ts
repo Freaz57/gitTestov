d0379324302c8d2701a3170f5d1ab15f482b2916
Ilya <ok.brain@yandex.ru>
2024-06-29 19:16:52 +0300

    git

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,7 +1,7 @@
 
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
-const TOKEN = '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'
+const TOKEN = ''
 const client = new ApolloClient({
     link: new HttpLink({
         uri: 'https://api.github.com/graphql',
